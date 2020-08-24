a2ba0ee20c69c15a0d12c3fc414ba354fcccefd0
Nathalie <nathaliemekahal@gmail.com>
2020-08-24 02:45:10 +0200

    messages style update

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -3,11 +3,16 @@ import "../Message.css";
 
 function Message({ user, userImage, message, timestamp }) {
   return (
-    <div className="Message">
+    <div className="message">
       {console.log(user)}
       <img src={userImage} />
       <div className="message__info">
-        <h4>{user} timestamp...</h4>
+        <h4>
+          {user}
+          <span className="message__date">
+            {new Date(timestamp?.toDate()).toUTCString()}
+          </span>
+        </h4>
         <p>{message}</p>
       </div>
     </div>
