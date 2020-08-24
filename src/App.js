28bca4f15bf66961e7fcb983b50e85f5b2aade00
Nathalie <nathaliemekahal@gmail.com>
2020-08-24 10:11:25 +0200

    login

--- src/App.js
+++ src/App.js
@@ -1,27 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/Header";
 import SideBar from "./components/SideBar";
 import SideBarOption from "./components/SideBarOption";
+import Login from "./components/Login";
 import "./App.css";
 import Chat from "./components/Chat";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
+  const [user, setUser] = useState(null);
   return (
     <div className="App">
       <Router>
-        <Header />
-        <div className="app__body">
-          <SideBar />
-          <Switch>
-            <Route path="/room/:roomId">
-              <Chat />
-            </Route>
-            <Route path="/">
-              <h1>Welcome to SlackClone</h1>
-            </Route>
-          </Switch>
-        </div>
+        <>
+          {!user && <Login />}
+          {user && (
+            <>
+              <Header />
+              <div className="app__body">
+                <SideBar />
+                <Switch>
+                  <Route path="/room/:roomId">
+                    <Chat />
+                  </Route>
+                  <Route path="/">
+                    <h1>Welcome to SlackClone</h1>
+                  </Route>
+                </Switch>
+              </div>
+            </>
+          )}
+        </>
       </Router>
     </div>
   );
